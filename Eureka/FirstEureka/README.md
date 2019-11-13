9fd2ac9a75f55131984bcb396db05b4783764eba
e <695200158@qq.com>
2019-11-13 14:35:54 +0800

    eureka1 finish

--- Eureka/FirstEureka/README.md
+++ Eureka/FirstEureka/README.md
@@ -1,1 +1,5 @@
 # 第一个Eureka例子
+- 服务本例：ek-server
+- 服务提供者ek-service-provider
+- 调用者ek-service-invoker
+- 通过9000端口router查找服务提供者的服务进行调用
---
+++ Eureka/FirstEureka/ek-service-invoker/src/main/java/com/ij34/cloud/TitleInvokerController.java
@@ -0,0 +1,29 @@
+package com.ij34.cloud;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Configuration
+public class TitleInvokerController {
+   
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+
+	  
+	     @RequestMapping(value="/router",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	 	public String getRouter(){
+	 		return getRestTemplate().getForObject("http://ek-service-provider/title/666", String.class);
+	 	}  
+	  
+}
