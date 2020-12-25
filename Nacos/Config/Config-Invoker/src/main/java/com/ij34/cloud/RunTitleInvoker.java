b0afb29d041911a7d02fff07af5556b1f4913250
sunday123 <695200158@qq.com>
2020-12-25 10:09:17 +0800

    springcloud config 远程 pull配置

---
+++ Nacos/Config/Config-Invoker/src/main/java/com/ij34/cloud/RunTitleInvoker.java
@@ -0,0 +1,17 @@
+package com.ij34.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RunTitleInvoker {
+
+   public static void main(String[] args) {
+	SpringApplication.run(RunTitleInvoker.class, args);
+}
+}
